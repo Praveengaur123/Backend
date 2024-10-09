e2b7c04e6a87059baba56f660604f0d49de4fe7a
Praveen Gaur <140590555+Praveengaur123@users.noreply.github.com>
2024-10-09 20:11:34 +0530

    creating server with port 4000

---
+++ app.js
@@ -0,0 +1,11 @@
+ const http=require('http');
+function rqListener(req,res){
+
+}
+
+ const server =http.createServer((req,res)=>{
+console.log(req);
+console.log('Praveen Gaur')
+
+ });
+ server.listen(4000)
