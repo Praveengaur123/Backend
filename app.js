bbbde07e4bb5c9cce44982b20b4ebc1158386af0
Praveen Gaur <140590555+Praveengaur123@users.noreply.github.com>
2024-10-10 22:03:13 +0530

    creating url

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
  const http=require('http');
-function rqListener(req,res){
+/*function rqListener(req,res){
 
-}
+}*/
 
  const server =http.createServer((req,res)=>{
-console.log(req);
-console.log('Praveen Gaur')
-
+//understanding the request
+console.log(req.url,req.method,req.headers);
+//console.log('Praveen Gaur')
+res.setHeader('content-type','text/html')
+/*res.write('<html>')
+res.write('<head><title>My First Page</title></head>')
+res.write('<body><h1>Hello from my node.js Server!</h1></body>')
+res.write('</html>')
+res.end();*/
+if(req.url==='/home') res.end('Welcome home');
+else if(req.url==='/about') res.end('Welcome to about us page');
+else if(req.url==='/node') res.end('Welcome to my Node.js project');
  });
  server.listen(4000)
